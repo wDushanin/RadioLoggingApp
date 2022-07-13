4cc18905e6c12d8f7f28c9c7287038810be6d629
wDushanin <wendy.dushanin@gmail.com>
2022-07-13 15:01:02 -0400

    adding code to use db functions in app

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
+import os
 from flask import Flask, abort, redirect, render_template, request
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+#can also use: bcrypt.init_app(app)
+
+db = SQLAlchemy(app)
+#can also use: db.init_app(app)
+
+#connecting to MySQL database
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
 @app.get('/')
