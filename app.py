06941dc8303bbed6044907082a321d3490902e6c
wDushanin <wendy.dushanin@gmail.com>
2022-07-17 18:27:12 -0400

    Working out Bugs

--- app.py
+++ app.py
@@ -1,4 +1,16 @@
+import os 
+from dotenv import load_dotenv
 from flask import Flask, abort, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.get('/')
 def index():
