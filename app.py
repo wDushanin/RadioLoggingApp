bf6176d9d60d2619ca74f6602c6a12c02db5a5d5
wDushanin <wendy.dushanin@gmail.com>
2022-07-17 18:29:57 -0400

    adding models import

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, abort, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from models import Log
 
 load_dotenv()
 
