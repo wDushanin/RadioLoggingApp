af0cd9d93222a6ca091691fdaaa40b8d9d56631b
wdushanin <wendy.dushanin@gmail.com>
2022-08-08 22:58:52 -0400

    adding routes to support search.html

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from flask import Flask, abort, redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from models import Log
+from webforms import SearchForm
 
 load_dotenv()
 
@@ -10,10 +11,29 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 db = SQLAlchemy(app)
 
+@app.context_processor
+def base():
+    form = SearchForm()
+    return dict(form=form)
+
 @app.get('/')
 def index():
+    #Get Logs from Database
     logs = Log.query.all()
     return render_template('index.html', logs=logs)
+
+# Create Search Function
+@app.route('/search', methods=["POST"])
+def search():
+    form = SearchForm()
+    logs = Log.query
+    if form.validate_on_submit():
+        log_searched = form.searched.data
+        #Query the Database
+        logs = logs.filter(Log.source_id.like(log_searched))
+        logs = logs.order_by(Log.time).all()
+        return render_template("search.html", form=form, searched=log_searched, logs=logs)
